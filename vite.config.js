f7887ee6e9b3d954f5d690ef54c4e37174b678fa
chencw <chencw@cniship.com>
2025-10-15 17:31:34 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    assetsDir: './dist',
+  },
   plugins: [vue()],
   resolve: {
     alias: {
