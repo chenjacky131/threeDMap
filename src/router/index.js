96596a3248ab3e65395bff6c8f845b216c1ff5bb
chencw <chencw@cniship.com>
2025-10-15 17:46:03 +0800

    fix：修改打包路径配置，修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/threeDMap/'),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: () => import('@/views/Home.vue'),
+      component: () => import('@/views/home.vue'),
     },
   ],
 })
