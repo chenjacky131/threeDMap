5fd0eb1e3145617512cb18e6a7c23745e70d5610
chencw <chencw@cniship.com>
2025-10-15 17:36:10 +0800

    fix：修改资源引入路径

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   build: {
-    assetsDir: './dist',
+    outDir: './dist',
   },
   plugins: [vue()],
   resolve: {
